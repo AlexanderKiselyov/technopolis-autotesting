64c2718f8d41e236b664f57ca29e097c44d8ee83
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2022-03-16 12:00:10 +0300

    BasePage fixes

--- src/java/main/pages/BasePage.java
+++ src/java/main/pages/BasePage.java
@@ -1,5 +1,13 @@
 package main.pages;
 
+import main.utils.ToolbarRight;
+
 public abstract class BasePage {
+    private final ToolbarRight toolbarRight = new ToolbarRight();
+
     public abstract boolean isPresent();
+
+    public void logout() {
+        toolbarRight.exitWithCheck();
+    }
 }
