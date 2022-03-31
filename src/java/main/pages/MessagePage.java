641303191599d69e3bd20842a574d3a77349f951
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2022-03-31 11:34:49 +0300

    Issue fixes

--- src/java/main/pages/MessagePage.java
+++ src/java/main/pages/MessagePage.java
@@ -3,6 +3,7 @@ package pages;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import java.util.Random;
 
 import static com.codeborne.selenide.Selectors.byCssSelector;
@@ -15,9 +16,8 @@ public class MessagePage extends BasePage {
     private final String MESSAGE_NEW_DIALOG_BUTTON = ".//msg-button[@data-tsid='open_plus_button']";
     private static final String MESSAGE_DIALOGS = "msg-parsed-text";
     private final String MESSAGE_INPUT_FIELD = ".//msg-input[@data-tsid='write_msg_input']";
-    private final String MESSAGE_LAST_SEND_MESSAGE = "msg-message";
     private final String MESSAGE_SETTINGS = ".//msg-button[@data-tsid='more_message']";
-    private final String MESSAGE_DELETE = ".//msg-tico[@icon='delete']";
+    private final String MESSAGE_DELETE = ".//msg-tico[@data-tsid='remove_msg_button']";
     private final String MESSAGE_TITLE = ".//*[contains(@key, 'title') and text()='Сообщения']";
     private static final ElementsCollection dialogs = $$(byCssSelector(MESSAGE_DIALOGS));
 
@@ -26,9 +26,20 @@ public class MessagePage extends BasePage {
         dialogs.shouldHave(CollectionCondition.sizeGreaterThan(1).because("No dialogs found!"));
     }
 
-    public void sendMessage(String message) {
+    private SelenideElement lastMessage(String message) {
+        return $(byXpath(".//*[text()='" + message + "']"));
+    }
+
+    public String generateMessage() {
+        return String.valueOf(System.currentTimeMillis());
+    }
+
+    public int generateDialogNum() {
         Random r = new Random();
-        int dialogNum = r.nextInt(dialogs.size());
+        return r.nextInt(dialogs.size());
+    }
+
+    public void sendMessage(int dialogNum, String message) {
         dialogs
                 .get(dialogNum)
                 .shouldBe(Condition.visible.because("No dialog with the specified number found!"))
@@ -39,17 +50,22 @@ public class MessagePage extends BasePage {
                 .pressEnter();
     }
 
-    // TODO
-    public void checkIfMessageSent() {
-
+    public void checkIfMessageSent(int dialogNum, String message) {
+        dialogs
+                .get(dialogNum).shouldBe(Condition.visible.because("No dialog with the specified number found!"))
+                .click();
+        lastMessage(message)
+                .shouldBe(Condition.visible.because("No messages found!"));
     }
 
-    public void deleteMessage(String message) {
+    // TODO
+    public void deleteMessage(int dialogNum, String message) {
         dialogs
-                .get(0)
+                .get(dialogNum)
                 .shouldBe(Condition.visible.because("No dialog with the specified number found!"))
-                .click();
-        $(byCssSelector(MESSAGE_LAST_SEND_MESSAGE))
+                .hover();
+
+        lastMessage(message)
                 .shouldBe(Condition.visible.because("No messages found!"))
                 .hover();
         $(byXpath(MESSAGE_SETTINGS))
@@ -60,9 +76,23 @@ public class MessagePage extends BasePage {
                 .click();
     }
 
-    // TODO
-    public void checkIfMessageDeleted() {
+    public void prepareMessageForDeleting(int dialogNum, String message) {
+        dialogs.get(dialogNum)
+                .shouldBe(Condition.visible.because("No dialog with the specified number found!"))
+                .click();
+        $(byXpath(MESSAGE_INPUT_FIELD))
+                .setValue(message)
+                .shouldBe(Condition.visible.because("No enter button found!"))
+                .pressEnter();
+    }
 
+    public void checkIfMessageDeleted(int dialogNum, String message) {
+        dialogs
+                .get(dialogNum)
+                .shouldBe(Condition.visible.because("No dialog with the specified number found!"))
+                .click();
+        lastMessage(message)
+                .shouldNotBe(Condition.visible.because("Message hasn't been deleted!"));
     }
 
     @Override
